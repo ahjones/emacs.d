445880158ec924f20868d190eb4a986f87302286
Andrew Jones <github@a4j.xyz>
2018-11-12 10:46:56 +0000

    Add God mode

--- init.el
+++ init.el
@@ -183,3 +183,26 @@
 
 (use-package terraform-mode
   :ensure t)
+
+(defun my-update-cursor ()
+  (setq cursor-type (if (or god-local-mode buffer-read-only)
+                        'box
+                      'bar)))
+
+(use-package god-mode
+  :ensure t
+  :config
+  (global-set-key (kbd "<escape>") 'god-mode-all)
+  (setq god-exempt-major-modes nil)
+  (setq god-exempt-predicates nil)
+  (add-hook 'god-mode-enabled-hook 'my-update-cursor)
+  (add-hook 'god-mode-disabled-hook 'my-update-cursor)
+  (require 'god-mode-isearch)
+  (define-key isearch-mode-map (kbd "<escape>") 'god-mode-isearch-activate)
+  (define-key god-mode-isearch-map (kbd "<escape>") 'god-mode-isearch-disable)
+  (global-set-key (kbd "C-x C-1") 'delete-other-windows)
+  (global-set-key (kbd "C-x C-2") 'split-window-below)
+  (global-set-key (kbd "C-x C-3") 'split-window-right)
+  (global-set-key (kbd "C-x C-0") 'delete-window)
+  (define-key god-local-mode-map (kbd ".") 'repeat))
+
